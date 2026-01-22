5b1edaa9064940583098a84423d1a929445a2970
Serhii Repinskyi <repinskyisv@gmail.com>
2026-01-22 21:25:29 +0200

    chore: configure main.ts and tasks entry point

---
+++ src/tasks.ts
@@ -0,0 +1,13 @@
+//  * Training tasks runner
+//  * Uncomment ONE task at a time
+
+// import './task-1';
+// import './task-2';
+// import './task-3';
+// import './task-4';
+// import './task-5';
+// import './task-6';
+// import './task-7';
+import "./task-8";
+
+console.log("%cRunning task-8", "color: green; font-weight: bold");
